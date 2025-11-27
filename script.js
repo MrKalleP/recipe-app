519953c8acedf6dd9af534805dd613b02f573f4e
kalle pettersson <kallle.v.pettersson@gmail.com>
2025-11-27 09:56:58 +0100

    fixed some responsive design and fixed js and json so its now working

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 async function GetRecepies() {
   const newRecipeContainer = document.getElementById('newRecipe-container');
 
-  const newRecipe = document.createElement('section');
-
   try {
-    const data = await fetch('./recepies.json');
-    const returnData = await data.json();
-    return returnData;
-  } catch (error) {
-    console.log(error, 'its one error here');
-  }
+    const response = await fetch('./recepies.json');
+    const data = await response.json();
 
-  for (let i = 0; i < returnData.length; i++) {
-    const recepieData = returnData[i];
-    newRecipe.innerHTML = `
-<img src="#">${recepieData.img}</img>
+    const recepies = data.recepies;
+
+    for (let i = 0; i < recepies.length; i++) {
+      const recepieData = recepies[i];
+
+      const newRecipe = document.createElement('section');
+
+      newRecipe.innerHTML = `
+<img src="${recepieData.img} alt=${recepieData.title}">
 <h3>${recepieData.title}</h3>
-<p></p>
+<p>${recepieData.description}</p>
 <ul>
-<li>Butter</li>
-<li>egg</li>
-<li>baking powder</li>
+<li>${[...recepieData.ingredients]}</li>
+<li>${[...recepieData.doThis]}</li>
+<li>${[...recepieData.forServing]}</li>
 <li>all purpose flower</li>
 </ul>
 `;
-    newRecipeContainer.appendChild(newRecipe);
+      newRecipeContainer.appendChild(newRecipe);
+    }
+  } catch (error) {
+    console.log(error, 'its one error here');
   }
 }
 
